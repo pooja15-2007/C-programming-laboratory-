2d9abb85b2f2d04381eff9b5ae2acf72154a1a19
pooja15-2007 <n.poojamaheswari@gmail.com>
2024-10-06 19:35:27 +0530

    Create Equalnum.c

---
+++ Equalnum.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() 
+{
+    int num1,num2;
+    printf("Enter the first integer: ");
+    scanf("%d", &num1);
+    printf("Enter the second integer: ");
+    scanf("%d", &num2);
+    if (num1 == num2) 
+    {
+        printf("The numbers are equal.\n");
+    }
+    else 
+    {
+        printf("The numbers are not equal.\n");
+    }
+    return 0;
+}
